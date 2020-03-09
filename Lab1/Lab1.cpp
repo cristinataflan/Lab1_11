711d61b714afd9a31b2a886067c2e5398763fb6b
cristinataflan <61684210+cristinataflan@users.noreply.github.com>
2020-03-09 10:50:52 +0200

    modificare1

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,25 +1,51 @@
 // Aufgabe 11
 //
-
+#include <cassert>
 #include <iostream>
 using namespace std;
 
+int prim(int x)
+{
+    if (x == 2)
+        return true;
+    if (x < 2 || !(x % 2))
+        return false;
+    else
+    for (int i = 2; i <x/2; i++)
+        if (!(x % i))
+            return false;
+    return true;
+}
+
+void test()
+{
+    assert(prim(9) == false);
+    assert(prim(5) == true);
+    assert(prim(2) == true);
+
+}
+
 void twinzahlen(int n) {
-    int p = 0, q=2;
-    //wir beginnen mit den ersten Paar und zwar 0 und 2
+    int p = 3, q=5, ct=0;
+    //wir beginnen mit den ersten Paar und zwar 3 und 5, wir testen ob die Zahlen prim sind
     //bei jeden Schritt p wird q bekommen und q wird mit zwei inkrementiert
-    for (int i = 0; i < n; i++)
+    while(ct<n)
     {
-        cout << "p=" << p << " ";
-        cout << "q=" << q << " ";
-        cout << endl;
-        p = q;
-        q += 2;
+        if (prim(p) && prim(q))
+        {
+            cout << "p=" << p << " ";
+            cout << "q=" << q << " ";
+            cout << endl;
+            ct++;
+        }
+            p = q;
+            q += 2;
+        
     }
 
 }
 
-void abnehmendeTeilfolge(int v[20], int n)
+void abnehmendeTeilfolge(int v[20], int n, int newliste[20])
 {
     int ct = 0, max = 0, position = 0,i=0;
     for (i = 0; i <= n - 1; i++)
@@ -42,12 +68,14 @@ void abnehmendeTeilfolge(int v[20], int n)
         max = ct;
         position = i - max;
          }
-   
-    for ( i = position; i <position+max; i++)
-        cout << v[i] << " ";
+    int j = 0;
+    for (i = position; i < position + max; i++)
+        newliste[j++] = v[i];
+
+    for (i = 0; i < max; i++)
+        cout << newliste[i] << " ";
    
 }
-
 
 
 int main()
@@ -55,9 +83,12 @@ int main()
     cout << "Punkt a:" << endl;
     twinzahlen(3);
     cout << "Punkt b:"<<endl;
-    int n=10;
+    int n=10, newliste[20];
     int v[] = {10,9,8,9,6,5,9,3,2,1};
-    
-    abnehmendeTeilfolge(v, 10);
+   
+    abnehmendeTeilfolge(v, 10, newliste);
+    test();
+
+  
 
 }
